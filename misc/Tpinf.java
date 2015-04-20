0b68ec57eba4b7452e98be135ed8c6e1da2c4def
fr0gs <fr0gs@bitbucket.org>
2015-04-20 19:26:51 +0200

    heh

--- misc/Tpinf.java
+++ misc/Tpinf.java
@@ -41,7 +41,7 @@ public class Main {
 			try (BufferedReader br = new BufferedReader(fr)) {
 				String line;	    	   
 			    int line_count = 0;
-			    double avg_tput = 0;
+			    double avg = 0;
 			    while ((line = br.readLine()) != null) {
 			    	line_count++;
 			    	
@@ -52,7 +52,7 @@ public class Main {
 			    	
 			    	temp = line.split(delimiter);
 			    	poskey = temp[1]; //second column
-			    	avg_tput += Double.parseDouble(poskey); 
+			    	avg += Double.parseDouble(poskey); 
 			    	delimiter = "\\.";
 			    	temp = poskey.split(delimiter);
 			    	poskey = temp[0];
@@ -72,11 +72,11 @@ public class Main {
 			    	double d = (double) val;
 			    	double percent =( d * 100.0)/(double)line_count;
 			    	DecimalFormat numberFormat = new DecimalFormat("#.00");
-			    	System.out.println("Key = " + entry.getKey() + " -- Percentage : " + numberFormat.format(percent));
+			    	System.out.println("Key = " + entry.getKey() + " -- Percentage : " + numberFormat.format(percent) + " %");
 			    }
-			    avg_tput = avg_tput/line_count;
+			    avg = avg/line_count;
 			    
-			    System.out.println("Average throughput = " + avg_tput);
+			    System.out.println("Average = " + avg);
 			}
 			catch (IOException ioe) {
 				System.err.println("tputstat: input/output error");
