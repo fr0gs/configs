293e4fb1a3a203dd0469c0e292d0dbc5309bc7b6
fr0gs <esteban.s.f0@gmail.com>
2018-09-04 20:18:48 +0200

    Added environment vars for pyenv

--- fish/config.fish
+++ fish/config.fish
@@ -1,22 +1,19 @@
-# Path to Oh My Fish install.
+### Path to Oh My Fish install.
 set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
+### Environment variables for rbenv (managing Ruby versions)
 set -gx PATH $PATH "$HOME/.rbenv/bin"
 set -gx PATH $PATH "$HOME/.rbenv/plugins/ruby-build/bin"
 
 # Load rbenv automatically by appending
 # the following to ~/.config/fish/config.fish:
-
 #status --is-interactive; and source (rbenv init -|psub)
 
-# For the colorscheme of omf (bobthefish)
-#https://github.com/oh-my-fish/theme-bobthefish
-set -g theme_powerline_fonts no
-set -g theme_color_scheme dark 
-set -g theme_display_date no
-
+### Environment variables for pyenv (managing Python versions)
+set -g -x PYENV_ROOT "$HOME/.pyenv"
+set -g -x PATH $PATH "$PYENV_ROOT/bin"
 
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
