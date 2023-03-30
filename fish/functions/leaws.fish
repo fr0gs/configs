1130bd6567af7b18b8e0b4d1bc8ecaad7b9149a9
Esteban <esteban.s.f0@gmail.com>
2023-03-30 14:00:12 +0200

    Load Environment AWS script fixed

--- fish/functions/leaws.fish
+++ fish/functions/leaws.fish
@@ -1,17 +1,15 @@
-# Load environment variables from aws based on the
-# profile specified.
-function leaws
+function leaws --argument profile --description="Load environment variables from aws based on the profile specified."
     # Set the aws binary path into a variable
     set -l awsbinary /usr/local/bin/aws
     if test -e $awsbinary
-        if set -q $argv
-            set -x AWS_ACCESS_KEY_ID (aws configure get aws_access_key_id)
-            set -x AWS_SECRET_ACCESS_KEY (aws configure get aws_secret_access_key)
-            set -x AWS_DEFAULT_REGION (aws configure get region)        
+        if set -q profile
+            set -gx AWS_ACCESS_KEY_ID (aws configure --profile $profile get aws_access_key_id)
+            set -gx AWS_SECRET_ACCESS_KEY (aws configure --profile $profile get aws_secret_access_key)
+            set -gx AWS_DEFAULT_REGION (aws configure --profile $profile get region)
         else
-            set -x AWS_ACCESS_KEY_ID (aws configure --profile $argv get aws_access_key_id)
-            set -x AWS_SECRET_ACCESS_KEY (aws configure --profile $argv get aws_secret_access_key)
-            set -x AWS_DEFAULT_REGION (aws configure --profile $argv get region)
+            set -gx AWS_ACCESS_KEY_ID (aws configure get aws_access_key_id)
+            set -gx AWS_SECRET_ACCESS_KEY (aws configure get aws_secret_access_key)
+            set -gx AWS_DEFAULT_REGION (aws configure get region)
         end
     else
 	    echo "aws cli not installed"
